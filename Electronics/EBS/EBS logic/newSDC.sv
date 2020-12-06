266bdacf06038e06ab99beced17483f20009ed5b
Roee douek <roydouek@gmail.com>
2020-12-06 11:11:50 +0200

    Add files via upload

--- Electronics/EBS/EBS logic/newSDC.sv
+++ Electronics/EBS/EBS logic/newSDC.sv
@@ -1,5 +1,5 @@
 module newSDC(
-    output logic To_SDC_relais,
+    output logic SDC_to_relais,
     output logic SDC_is_Ready,
     input logic AS_close_SDC,
     input logic AS_driving_mode,
@@ -10,48 +10,27 @@ module newSDC(
     input logic Power_on_Reset
 );
 
+logic nandout = 0;
+logic nandwatch = 0;
+logic muxout = 0;
+logic K1Out1 = 0;
+logic K2out = 0;
+logic K1Qbar = 1;
+logic K2Rin = 0;
+SR_NAND_latch K1 (.S(~nandout), .R(Power_on_Reset)  , .C(0) , .Q(K1Out1) , .Qbar(K1Qbar));
+SR_NAND_latch K2 (.S(Power_on_Reset), .R(~K2Rin)   , .C(0) , .Q(K2out));
+mux2 mux (.Z(muxout), .d0(TS_Activation_Button_cockpit), .d1(TS_Activation_Button_external), .sel(AS_driving_mode));
 
-logic Activation;
-logic w[3:0];
-logic k1_S;
-logic k1_R;
-logic k1_Q;
-
-logic k2_S;
-logic k2_R;
-logic k2_Q;
 
 always_comb
 begin
-    Activation = (AS_driving_mode == 1'b1) ? TS_Activation_Button_external :
-                TS_Activation_Button_cockpit;
-    w[0] = AS_close_SDC & Activation;
-    w[1] = Watchdog & Shutdown_circuit;
-    w[2] = (k1_Q | w[1]);
-    k1_R = Power_on_Reset;
-    k2_S = Power_on_Reset;
-    k1_S = w[0];
-    k2_R = ~w[2];
+	nandout = ~(muxout & AS_close_SDC);
+	nandwatch = (Shutdown_circuit & Watchdog);
+	K2Rin = (nandwatch | ~K1Qbar);
+	SDC_is_Ready = (nandwatch & K2out);
+	SDC_to_relais = (K1Out1 & K2out);
 end
 
-
-always_ff @(posedge k1_R or posedge k1_S)
-begin
-    if(k1_R) k1_Q <= 1'b0;
-    else if(k1_S) k1_Q <= 1'b1;
-end
-
-always_ff @(posedge k2_R or posedge k2_S)
-begin
-    if(k2_R) k2_Q <= 1'b1;
-    else if(k2_S) k2_Q <= 1'b0;
-end
-
-always_comb
-begin
-    To_SDC_relais = k1_Q & k2_Q;
-    SDC_is_Ready = k2_Q & w[1];
-end
 
 
 endmodule
